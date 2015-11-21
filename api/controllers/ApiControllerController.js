c1bd1c3978ac9bfed53ff7ffa513fed8cc30bb7c
infloop <infloop.gm@gmail.com>
2015-11-21 12:29:01 +0300

    First commit

---
+++ api/controllers/ApiControllerController.js
@@ -0,0 +1,11 @@
+/**
+ * ApiController
+ *
+ * @description :: Server-side logic for managing Api
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
---
+++ api/controllers/UserController.js
@@ -0,0 +1,12 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Api
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+var UserController = module.exports = {
+  create: function(req,res) {
+
+  }
+};
